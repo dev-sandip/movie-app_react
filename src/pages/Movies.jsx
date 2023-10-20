9572a65e660b50b31e96ddaf8c5adc7722115b97
Sandip Sapkota <079bct076@ioepc.edu.np>
2023-10-20 13:36:53 +0545

    fixed some issues

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,5 +1,23 @@
+import { useGlobalContext } from "../context";
+
 const Movies = () => {
-  return <div>Movies</div>;
+  const { movie } = useGlobalContext();
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      {movie.map((movie) => (
+        <div
+          key={movie.imdbID}
+          className="max-w-sm rounded overflow-hidden shadow-lg"
+        >
+          <img src={movie.Poster} alt={movie.Title} className="w-full" />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{movie.Title}</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
